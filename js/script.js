e733265109db7e0d0b17c35ecbfeb0df58aa714c
Abdullah Rahman <abdullahrahh@gmail.com>
2019-10-16 09:09:12 -0700

    added new features for flickity

---
+++ js/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() { 
+ console.log("hello world");
+document.getElementById("sign-up-form").addEventListener("submit", function() {
+
+});
+
+
+// All of your code will go here//
+const elem = document.querySelector('.main-carousel');
+const flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  wrapAround: true,
+  freeScroll: true
+});
+});// end of the ready doc
