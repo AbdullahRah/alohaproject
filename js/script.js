3d7b57a452dffa684d0bf83e82091ce5d15c31fd
Abdullah Rahman <abdullahrahh@gmail.com>
2019-11-27 17:51:24 -0800

    media queries

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
-document.addEventListener("DOMContentLoaded", function() { 
- console.log("hello world");
-document.getElementById("sign-up-form").addEventListener("submit", function() {
+$(document).ready(function() {
+  // All of your code will go here//
+  const elem = document.querySelector(".main-carousel");
+  const flkty = new Flickity(elem, {
+    // options
+    cellAlign: "left",
+    wrapAround: true,
+    freeScroll: true
+  });
+  $(".carouse-cell").append('<button class="add-item">Add to Cart</button>');
 
+  $("#subscribe").on("click", function(e) {
+    if ($("#your-email").val() == null || $("#your-email").val() == undefined) {
+      $("#your-email").focus();
+      alert("Please fill the required input at the Email Textbox");
+      return;
+    } else {
+      alert("Email Sent, Thank you for Subscribing!");
+    }
+  });
+
+  e.preventDefault();
 });
-
-
-// All of your code will go here//
-const elem = document.querySelector('.main-carousel');
-const flkty = new Flickity( elem, {
-  // options
-  cellAlign: 'left',
-  wrapAround: true,
-  freeScroll: true
-});
-});// end of the ready doc
